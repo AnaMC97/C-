8802e123e918018c45f22b270cf954dc1dba7ad7
Ana Coelho <ana.mcoelho97@gmail.com>
2022-10-04 15:56:34 +0100

    Exercicio 1 em C#

---
+++ Aula_1_Csharp/Aula_1_Csharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Aula1Csharp
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercicio 1.1//
+            Console.WriteLine("Exercicio 1.1");
+            Console.WriteLine("Hello!");
+            Console.WriteLine("Ana Coelho");
+            Console.WriteLine(" ");
+            //Exercicio 1.2//
+            Console.WriteLine("Exercicio 1.2");
+            int n, n1, soma;
+            n = 3;
+            n1 = 5;
+            soma = n + n1;
+            Console.WriteLine("A soma é: "+soma);
+            Console.WriteLine(" ");
+            //Exercicio 1.3//
+            Console.WriteLine("Exercicio 1.3");
+            string n2;
+            Console.WriteLine("Introduza um valor: ");
+            n2 = Console.ReadLine();
+            Console.WriteLine(n2);
+            Console.WriteLine(" ");
+            //Exercicio 2.1//
+            Console.WriteLine("Exercicio 2.1");
+            string n3, n4;
+            Console.WriteLine("Introduza um valor: "); 
+            n3 = Console.ReadLine();
+            Console.WriteLine("Introduza outro valor: ");
+            n4 = Console.ReadLine();
+            Console.WriteLine(n4 +" "+ n3);
+            //Exercicio 2.2//
+
+
+        }
+    }
+}
